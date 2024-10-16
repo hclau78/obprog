abdd2f176963ac769814cd3669654fc6e17196fe
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-16 21:47:06 +0200

    Update repulo_foglalas.py
    osztalyok lketrehozasa

--- repulo_foglalas.py
+++ repulo_foglalas.py
@@ -1,18 +1,36 @@
 class Jarat:
-    def __init__(self, jaratszam, cel, indulas, repulesido, jegyar):
+    def __init__(self, jaratszam, celorszag, celvaros, indulasido, repulesido, jegyar):
         self.jaratszam = jaratszam
-        self.cel = cel
-        self.indulas = indulas
+        self.celorszag = celorszag
+        self.celvaros = celvaros
+        self.indulas = indulasido
         self.repulesido = repulesido
         self.jegyar = jegyar
 
-jarat1 = Jarat('HU1225', 'Debrecen', '12:35', '1 óra',  ' 12 500 Ft')
-jarat2 = Jarat('FR5674', 'Parizs', '21:00', '3 óra', '56 000 Ft')
-jarat3 = Jarat('MAN8412', 'Manila', '6:00', '15 óra', '128 900 Ft')
+jarat1 = Jarat('HU1225',  'Magyarorszag', 'Debrecen', '12:35', '1 óra',  ' 12 500 Ft')
+jarat2 = Jarat('FR5674',  'Franciaorszag','Parizs', '21:00', '3 óra', '56 000 Ft')
+jarat3 = Jarat('MAN8412',  'Fulop-szihetek', 'Manila', '6:00', '15 óra', '128 900 Ft')
 
 class Foglalas:
     def __init__(self, vezeteknev, keresztnev, jarat):
         self.vezeteknev = vezeteknev
         self.keresztnev = keresztnev
         self.jarat = jarat
+
+class BelfoldiJarat:
+    def __init__(self, celorszag, repulesido, jegyar):
+        self.celorszag = celorszag
+        self.repulesido = repulesido
+        self.jegyar = jegyar
+
+class NemzetkoziJarat:
+    def __init__(self, celorszag, jegyar):
+        self.celorszag = celorszag
+        self.jegyar = jegyar
+
+class LegiTarsasag:
+    def __init__(self, legitarsasag):
+        self.legitarsasag = legitarsasag
+
+
 
