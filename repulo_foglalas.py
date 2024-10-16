c2779d2c80558bf8bb1f4ebf8e1d395e6078f1a5
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-16 21:49:40 +0200

    Update repulo_foglalas.py
    felesleges szokozok torlese

--- repulo_foglalas.py
+++ repulo_foglalas.py
@@ -7,9 +7,9 @@ class Jarat:
         self.repulesido = repulesido
         self.jegyar = jegyar
 
-jarat1 = Jarat('HU1225',  'Magyarorszag', 'Debrecen', '12:35', '1 óra',  ' 12 500 Ft')
-jarat2 = Jarat('FR5674',  'Franciaorszag','Parizs', '21:00', '3 óra', '56 000 Ft')
-jarat3 = Jarat('MAN8412',  'Fulop-szihetek', 'Manila', '6:00', '15 óra', '128 900 Ft')
+jarat1 = Jarat('HU1225', 'Magyarorszag', 'Debrecen', '12:35', '1 óra', ' 12 500 Ft')
+jarat2 = Jarat('FR5674', 'Franciaorszag','Parizs', '21:00', '3 óra', '56 000 Ft')
+jarat3 = Jarat('MAN8412', 'Fulop-szihetek', 'Manila', '6:00', '15 óra', '128 900 Ft')
 
 class Foglalas:
     def __init__(self, vezeteknev, keresztnev, jarat):
