48a7c9818984560555fd9faa3a27916706adaa8f
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-26 16:16:12 +0200

    Update alap_adatok.py

--- alap_adatok.py
+++ alap_adatok.py
@@ -1,1 +1,10 @@
-jar
+jarat1 = Jarat('1', 'Magyarorszag', '10000')
+jarat2 = Jarat('2', 'Franciaorszag', '20000')
+jarat3 = Jarat('3',  'Fulop-szigetek',  '50000')
+
+foglalas1 = Foglalas("Malacka", jarat1)
+foglalas2 = Foglalas("Fules", jarat1)
+foglalas3 = Foglalas('Tigris', jarat2)
+foglalas4 = Foglalas('Zsebi baba', jarat2)
+foglalas5 = Foglalas('Nyuszi', jarat2)
+foglalas6 = Foglalas('Micimacko', jarat3)
