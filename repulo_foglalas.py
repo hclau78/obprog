2cf4369e96e7265b9ff6c620520d6fe8de3308d2
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-26 18:03:38 +0200

    Update repulo_foglalas.py

--- repulo_foglalas.py
+++ repulo_foglalas.py
@@ -3,16 +3,7 @@ import belfoldi_jarat
 import nemzetkozi_jarat
 import foglalas
 import legitarsasag
+import menu
 
-class Foglalas:
-    def __init__(self, jarat, utas_nev):
-        self.jarat = jarat
-        self.utas_nev = utas_nev
-
-
-class LegiTarsasag:
-    def __init__(self, nev):
-        self.nev = nev
-        self.jaratok = []
-        self.foglalasok = []
+repulo_foglalas()
 
