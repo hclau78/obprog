d54751e6cb7494087297989dc9cb8353443fc940
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-26 19:59:07 +0200

    Update alap_adatok.py

--- alap_adatok.py
+++ alap_adatok.py
@@ -1,10 +1,12 @@
-jarat1 = Jarat('1', 'Magyarorszag', '10000')
-jarat2 = Jarat('2', 'Franciaorszag', '20000')
-jarat3 = Jarat('3', 'Fulop-szigetek', '50000')
+import BelfoldiJarat
 
-foglalas1 = Foglalas("Malacka", jarat1)
-foglalas2 = Foglalas("Fules", jarat1)
-foglalas3 = Foglalas('Tigris', jarat2)
-foglalas4 = Foglalas('Zsebi baba', jarat2)
-foglalas5 = Foglalas('Nyuszi', jarat2)
-foglalas6 = Foglalas('Micimacko', jarat3)
+legi_tarsasag.jarat_hozzaadasa(BelfoldiJarat( "B001", 'Magyarorszag', '10000'))
+#jarat2 = Jarat('2', 'Franciaorszag', '20000')
+#jarat3 = Jarat('3', 'Fulop-szigetek', '50000')
+
+#foglalas1 = Foglalas("Malacka", jarat1)
+#foglalas2 = Foglalas("Fules", jarat1)
+#foglalas3 = Foglalas('Tigris', jarat2)
+#foglalas4 = Foglalas('Zsebi baba', jarat2)
+#foglalas5 = Foglalas('Nyuszi', jarat2)
+#foglalas6 = Foglalas('Micimacko', jarat3)
