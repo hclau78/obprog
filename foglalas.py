cd02368d704fb4156d9e13176502591791d8f0a8
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-26 19:59:36 +0200

    Update foglalas.py

--- foglalas.py
+++ foglalas.py
@@ -2,3 +2,9 @@ class Foglalas:
     def __init__(self, jarat, utas_nev):
         self.jarat = jarat
         self.utas_nev = utas_nev
+
+    def foglalas_info(self):
+        return f"{self._utas_nev} foglalt egy jegyet a(z) {self.jarat.jaratszam} számú járatra ({self.jarat.celallomas})"
+
+    def jegyar(self):
+        return self.jarat.jegyar
