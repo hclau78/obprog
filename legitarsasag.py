181af1dcd1bfa7fa8df56fb143f9590d14eb54f2
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-26 19:59:49 +0200

    Update legitarsasag.py

--- legitarsasag.py
+++ legitarsasag.py
@@ -3,3 +3,46 @@ class LegiTarsasag:
         self.nev = nev
         self.jaratok = []
         self.foglalasok = []
+
+    def jarat_hozzaadasa(self, jarat):
+        self.jaratok.append(jarat)
+
+    def jegy_foglalasa(self, jaratszam, utas_nev):
+        jarat = self._keres_jarat(jaratszam)
+        if jarat:
+            foglalas = JegyFoglalas(jarat, utas_nev)
+            self.foglalasok.append(foglalas)
+            print(f"Sikeres foglalás: {foglalas.foglalas_info()}")
+            return foglalas.jegyar()
+        else:
+            print("Nincs ilyen járat.")
+            return None
+
+    def foglalas_lemondasa(self, jaratszam, utas_nev):
+        for foglalas in self.foglalasok:
+            if foglalas.jarat.jaratszam == jaratszam and foglalas.utas_nev == utas_nev:
+                self.foglalasok.remove(foglalas)
+                print(f"{utas_nev} lemondta a(z) {jaratszam} számú járatra szóló foglalását.")
+                return True
+        print("Nincs ilyen foglalás.")
+        return False
+
+    def foglalasok_listazasa(self):
+        if not self.foglalasok:
+            print("Nincsenek aktuális foglalások.")
+        else:
+            for foglalas in self.foglalasok:
+                print(foglalas.foglalas_info())
+
+    def jaratok_listazasa(self):
+        if not self.jaratok:
+            print("Nincsenek járatok.")
+        else:
+            for jarat in self.jaratok:
+                print(jarat.jarat_info())
+
+    def _keres_jarat(self, jaratszam):
+        for jarat in self.jaratok:
+            if jarat.jaratszam == jaratszam:
+                return jarat
+        return None
