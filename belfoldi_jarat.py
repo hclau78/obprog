7d4ea40935039fa5e3d6d5bf49de88c4982f149e
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-27 19:27:11 +0100

    Update belfoldi_jarat.py

--- belfoldi_jarat.py
+++ belfoldi_jarat.py
@@ -3,9 +3,7 @@ from jarat import Jarat
 
 class BelfoldiJarat(Jarat):
     def __init__(self, jaratszam, celallomas, jegyar):
-        self.jaratszam = jaratszam
-        self.celallomas = celallomas
-        self.jegyar = jegyar
+        super().__init__(jaratszam, celallomas, jegyar)
 
     def jarat_info(self):
         return f"Belföldi járat: {self.jaratszam}, Célállomás: {self.celallomas}, Jegyár: {self.jegyar} Ft"
