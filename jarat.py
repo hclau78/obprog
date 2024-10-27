ecf58fee6dced62ce5d53927bd46206cef02b856
hclau78 <69871773+hclau78@users.noreply.github.com>
2024-10-27 19:27:58 +0100

    Update jarat.py

--- jarat.py
+++ jarat.py
@@ -4,9 +4,21 @@ from abc import abstractmethod, ABC
 
 class Jarat(ABC):
     def __init__(self, jaratszam, celallomas, jegyar):
-        self.jaratszam = jaratszam
-        self.celallomas = celallomas
-        self.jegyar = jegyar
+        self._jaratszam = jaratszam
+        self._celallomas = celallomas
+        self._jegyar = jegyar
+
+    @property
+    def jaratszam(self):
+        return self._jaratszam
+
+    @property
+    def celallomas(self):
+        return self._celallomas
+
+    @property
+    def jegyar(self):
+        return self._jegyar
 
     @abstractmethod
     def jarat_info(self):
